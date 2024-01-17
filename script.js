35b5d762efc99fb05d4b58ded1839c18832bd706
nallujin <benedictuspujagarfield@gmail.com>
2024-01-17 19:46:26 +0700

    Create the divs & Set up a hover effect

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const container = document.querySelector(".container");
+for (let i = 1; i <= 273; i++) { // 256 + 16
+    const div = document.createElement("div");
+    div.classList.add("masbro");
+    container.appendChild(div);
+}
 
+const divChildren = document.querySelectorAll(".container>div");
+
+divChildren.forEach((divChild) => {
+    divChild.addEventListener("mouseenter", () => {
+        console.log("hello");
+        divChild.classList.add("hoverboard");
+    });
+});
